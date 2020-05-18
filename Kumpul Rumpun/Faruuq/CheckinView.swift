1fc001d195eaedf636f1b920c7443b37630ef55e
Faruuq <faruuq.q@icloud.com>
2020-05-18 17:29:03 +0800

    add new viewcontroller

---
+++ Kumpul Rumpun/Faruuq/CheckinView.swift
@@ -0,0 +1,16 @@
+//
+//  CheckinView.swift
+//  Kumpul Rumpun
+//
+//  Created by Muhammad Faruuq Qayyum on 18/05/20.
+//  Copyright © 2020 Apple Academy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CheckinView: UIViewController {
+    override func viewDidLoad() {
+        <#code#>
+    }
+}
