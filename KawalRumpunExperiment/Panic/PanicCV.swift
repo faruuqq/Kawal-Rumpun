bcd90045aba77a084b14b97de2f0dce7652201cd
Rais Mohamad Najib <raismnajib@gmail.com>
2020-05-21 23:47:21 +0700

    Make UiLabel

---
+++ KawalRumpunExperiment/Panic/PanicCV.swift
@@ -0,0 +1,13 @@
+//
+//  PanicCV.swift
+//  KawalRumpunExperiment
+//
+//  Created by Rais Mohamad Najib on 21/05/20.
+//  Copyright © 2020 Muhammad Fawwaz Mayda. All rights reserved.
+//
+
+import UIKit
+
+class PanicCV: UICollectionViewCell {
+    
+}
