761cf048c5f1141ea04d6b819d243a4b900555fd
fawwazmayda <maydafawwaz@gmail.com>
2020-05-21 14:06:04 +0800

    using UserDefaults for checking the id_warga

--- KawalRumpunExperiment/Report(Formely Complaint)/ReportFirstVC.swift
+++ KawalRumpunExperiment/Report(Formely Complaint)/ReportFirstVC.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
+struct ReportModel {
+    var date : Date = Date.
+    var keluhanType = ""
+    var complaintText = ""
+}
 class ReportFirstVC: UIViewController {
-
+    var currentIdWarga = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //Asume we have the user already logged in
+        if UserDefaults.standard.string(forKey: "id_warga")?.isEmpty {
+            currentIdWarga = "test"
+        } else {
+            currentIdWarga = UserDefaults.standard.string(forKey: "id_warga")
+        }
         // Do any additional setup after loading the view.
     }
     
