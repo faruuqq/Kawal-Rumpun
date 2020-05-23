7dcb1b6e23dd51a264741e8a2564e2563d079147
fawwazmayda <maydafawwaz@gmail.com>
2020-05-23 11:56:57 +0800

    Table view for Heatlh Check

---
+++ KawalRumpunExperiment/HealthCheck/HealthCheckCell.swift
@@ -0,0 +1,28 @@
+//
+//  HealthCheckCell.swift
+//  KawalRumpunExperiment
+//
+//  Created by Muhammad Fawwaz Mayda on 23/05/20.
+//  Copyright © 2020 Muhammad Fawwaz Mayda. All rights reserved.
+//
+
+import UIKit
+
+class HealthCheckCell: UITableViewCell {
+
+    @IBOutlet weak var healthCheckListLabel: UILabel!
+    @IBOutlet weak var tickImageView: UIImageView!
+    static let reuseIdentifier = String(describing: HealthCheckCell.self)
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- KawalRumpunExperiment/HealthCheck/HealthCheckFirstVC.swift
+++ KawalRumpunExperiment/HealthCheck/HealthCheckFirstVC.swift
@@ -7,28 +7,62 @@
 //
 
 import UIKit
-
+struct HealthCheckItem {
+    var healthCheckStatusLabel : String = ""
+    var healthCheckStatus : Bool = false
+    init(healthCheckStatusLabel: String, healthCheckStatus: Bool) {
+        self.healthCheckStatus = healthCheckStatus
+        self.healthCheckStatusLabel = healthCheckStatusLabel
+    }
+    
+    mutating func toggle() {
+        self.healthCheckStatus = !self.healthCheckStatus
+    }
+}
 class HealthCheckFirstVC: UIViewController {
     
     @IBOutlet weak var todayLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
-    
+    var dataSource = [HealthCheckItem]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
+        populateData()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func populateData() {
+        dataSource.append(HealthCheckItem(healthCheckStatusLabel: "Body Temperature > 37.5 C", healthCheckStatus: false))
+        dataSource.append(HealthCheckItem(healthCheckStatusLabel: "Dizzy", healthCheckStatus: false))
     }
-    */
+    
+    func configureCheckMark(for cell: HealthCheckCell, indexPath: IndexPath) {
+        cell.healthCheckListLabel.text = dataSource[indexPath.row].healthCheckStatusLabel
+        if dataSource[indexPath.row].healthCheckStatus {
+            cell.tickImageView.image = UIImage(systemName: "checkmark.rectangle.fill")
+        } else {
+            cell.tickImageView.image = UIImage(systemName: "trash")
+        }
+    }
+}
 
+extension HealthCheckFirstVC: UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: HealthCheckCell.reuseIdentifier, for: indexPath) as? HealthCheckCell else {fatalError("Fail to get the cell")}
+        configureCheckMark(for: cell, indexPath: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        dataSource[indexPath.row].toggle()
+        guard let selectedCell = tableView.cellForRow(at: indexPath) as? HealthCheckCell else {fatalError("Cant convert cell")}
+        configureCheckMark(for: selectedCell, indexPath: indexPath)
+    }
+    
 }
