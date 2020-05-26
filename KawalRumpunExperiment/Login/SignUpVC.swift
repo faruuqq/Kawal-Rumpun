454b09ff1695243324c8cbe9d8683434967cf181
Firza Ilhami <wongacid@gmail.com>
2020-05-26 16:32:14 +0700

    1. finishing signup UI component 2. adding outlet for signup uiviewcontroller

--- KawalRumpunExperiment/Login/SignUpVC.swift
+++ KawalRumpunExperiment/Login/SignUpVC.swift
@@ -10,8 +10,24 @@ import UIKit
 
 class SignUpVC: UIViewController {
 
+    
+    @IBOutlet weak var namaTF: UITextField!
+    @IBOutlet weak var emailRegisterTF: UITextField!
+    @IBOutlet weak var noHPTF: UITextField!
+    @IBOutlet weak var alamatTF: UITextField!
+    @IBOutlet weak var alamatRTTF: UITextField!
+    @IBOutlet weak var alamatRWTF: UITextField!
+    @IBOutlet weak var inputPassword1TF: UITextField!
+    @IBOutlet weak var inputPassword2TF: UITextField!
+    @IBOutlet weak var shareLocSwitch: UISwitch!
+    @IBOutlet weak var emailKeluargaTF: UITextField!
+    @IBOutlet weak var listKeluargaTable: UITableView!
+    
+    let listKeluarga = ["Ayah", "Ibu", "Saudara Laki-laki", "Saudara Perempuan", "Lainya"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        listKeluargaTable.delegate = self
+        listKeluargaTable.dataSource = self
 
         // Do any additional setup after loading the view.
     }
@@ -27,4 +43,19 @@ class SignUpVC: UIViewController {
     }
     */
 
+}
+
+extension SignUpVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listKeluarga.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = listKeluargaTable.dequeueReusableCell(withIdentifier: "statusKeluargaCell", for: indexPath)
+        cell.textLabel?.text = listKeluarga[indexPath.row]
+        return cell
+    }
+    
+    
 }
