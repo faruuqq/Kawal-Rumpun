4b4e793c23074fcdcf5ee9c99e7969be831b101a
Rais Mohamad Najib <raismnajib@gmail.com>
2020-05-26 16:37:56 +0700

    Add IBOutlet Report Cell

---
+++ KawalRumpunExperiment/Messages/ReportCVCell.swift
@@ -0,0 +1,13 @@
+//
+//  ReportCVCell.swift
+//  KawalRumpunExperiment
+//
+//  Created by Rais Mohamad Najib on 26/05/20.
+//  Copyright © 2020 Muhammad Fawwaz Mayda. All rights reserved.
+//
+
+import UIKit
+
+class ReportCVCell: UICollectionViewCell {
+    
+}
