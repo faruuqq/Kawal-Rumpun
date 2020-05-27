6552dc8356932df8a05c1765babff9e0b29a43e9
Rais Mohamad Najib <raismnajib@gmail.com>
2020-05-27 16:14:20 +0700

    skeleton view Colloectopn Controller

---
+++ KawalRumpunExperiment/Messages/HealthCheckCVCell.swift
@@ -0,0 +1,13 @@
+//
+//  HealthCheckCVCell.swift
+//  KawalRumpunExperiment
+//
+//  Created by Rais Mohamad Najib on 27/05/20.
+//  Copyright © 2020 Muhammad Fawwaz Mayda. All rights reserved.
+//
+
+import UIKit
+
+class HealthCheckCVCell: UICollectionViewCell {
+    
+}
