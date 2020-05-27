e4308301fed647028adc89a2169e1f931d183750
Rais Mohamad Najib <raismnajib@gmail.com>
2020-05-27 23:20:12 +0700

    check additional

--- KawalRumpunExperiment/Messages/HealtchCheckLabelTVCell.swift
+++ KawalRumpunExperiment/Messages/HealtchCheckLabelTVCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class HealtchCheckLabelTVCell: UITableViewCell {
 
+    @IBOutlet weak var healtchCheckListLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
